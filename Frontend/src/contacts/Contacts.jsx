0171158d8367592310803aa8676fe9bb0ef02ead
Anup Kumar <rachitjha999@gmail.com>
2024-07-20 10:23:29 +0530

    Frontend Done!!

---
+++ Frontend/src/contacts/Contacts.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Contact from "../components/Contact";
+
+function Contacts() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Contact />
+      </div>
+      con
+    </>
+  );
+}
+export default Contacts;
