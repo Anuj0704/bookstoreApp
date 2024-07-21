dd36c168de8a03286dd068060a38c9d5db4237a5
Anup Kumar <rachitjha999@gmail.com>
2024-07-21 12:46:41 +0530

    second commit

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,62 +16,62 @@ function Login() {
           <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <Link
-                to="/"
-                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-              >
-                ✕
-              </Link>
-         
-          <h3 className="font-bold text-lg">Login</h3>
-          {/**Email */}
-          <div className="mt-4  space-y-2">
-            <span>Email</span>
-            <br />
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-80 px-3 py-1 border rounded outline-none"
-              {...register("email", { required: true })}
-            />
-            <br />
-            {errors.email && (
-              <span className="text-sm text-red-500">
-                This field is required
-              </span>
-            )}
-          </div>
-          {/**Password */}
-          <div className="mt-4  space-y-2">
-            <span>Password</span>
-            <br />
-            <input
-              type="text"
-              placeholder="Enter your password"
-              className="w-80 px-3 py-1 border rounded outline-none"
-              {...register("password", { required: true })}
-            />
-            <br />
-            {errors.password && (
-              <span className="text-sm text-red-500">
-                This field is required
-              </span>
-            )}
-          </div>
-          {/**Button */}
-          <div className="flex justify-around mt-4">
-            <button className="bg-pink-500 dark:bg-pink-600 text-white rounded-md px-3 py-1 hover:bg-pink-700 dark:hover:bg-pink-800  duration-200">
-              Login
-            </button>
-            <p>
-              Not registered?{" "}
-              <Link
-                to="/signup"
-                className="underline text-blue-500 cursor-pointer"
-              >
-                Signup
-              </Link>{" "}
-            </p>
-          </div>
+              to="/"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            >
+              ✕
+            </Link>
+
+            <h3 className="font-bold text-lg">Login</h3>
+            {/**Email */}
+            <div className="mt-4  space-y-2">
+              <span>Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded outline-none"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
+            </div>
+            {/**Password */}
+            <div className="mt-4  space-y-2">
+              <span>Password</span>
+              <br />
+              <input
+                type="text"
+                placeholder="Enter your password"
+                className="w-80 px-3 py-1 border rounded outline-none"
+                {...register("password", { required: true })}
+              />
+              <br />
+              {errors.password && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
+            </div>
+            {/**Button */}
+            <div className="flex justify-around mt-4">
+              <button className="bg-pink-500 dark:bg-pink-600 text-white rounded-md px-3 py-1 hover:bg-pink-700 dark:hover:bg-pink-800  duration-200">
+                Login
+              </button>
+              <p>
+                Not registered?{" "}
+                <Link
+                  to="/signup"
+                  className="underline text-blue-500 cursor-pointer"
+                >
+                  Signup
+                </Link>{" "}
+              </p>
+            </div>
           </form>
         </div>
       </dialog>
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Login from "./Login";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 function Signup() {
   const {
@@ -9,7 +10,29 @@ function Signup() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      fullname: data.fullname,
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("http://localhost:4001/user/signup", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          alert("Signup Successfully");
+        }
+      })
+      .catch((err) => {
+        if(err.reponse){
+          console.log(err);
+          alert("Error: "+ err.response.data.message)
+        }    
+      });
+  };
+
   return (
     <>
       <div className="flex h-screen items-center justify-center ">
@@ -33,10 +56,10 @@ function Signup() {
                   type="text"
                   placeholder="Enter your fullname"
                   className="w-80 px-3 py-1 border rounded outline-none"
-                  {...register("name", { required: true })}
+                  {...register("fullname", { required: true })}
                 />
                 <br />
-                {errors.name && (
+                {errors.fullname && (
                   <span className="text-sm text-red-500">
                     This field is required
                   </span>
