1847001ec265a0f14384f1da6b1199e6528ba2b7
Anuj0704 <121963981+Anuj0704@users.noreply.github.com>
2024-08-12 01:04:28 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -4,11 +4,12 @@ import Login from "./Login";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import toast from "react-hot-toast";
+const url = "https://bookstoreapp-backend-8aa9.onrender.com";
 
 function Signup() {
   const location = useLocation();
   const navigate = useNavigate();
-  const url = "https://bookstoreapp-backend-8aa9.onrender.com";
+  
   const from = location.state?.from.pathname || "/";
   const {
     register,
